284c6f2ddbef2d29f534c04544ab1e6a5ff4ad1b
Paul Sutcliffe <frpaulas@gmail.com>
2018-03-13 11:39:34 -0400

    try new config

--- lib/iphod_web/IphodWeb.endpoint.ex
+++ lib/iphod_web/IphodWeb.endpoint.ex
@@ -9,7 +9,7 @@ defmodule IphodWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :iphod, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
+    only: ~w(assets css fonts images js favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
